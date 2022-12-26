769bc02f9d649e0fd60a61b1a286547c92e8e041
RicoC-WU <rjcoleman@wustl.edu>
2022-12-25 20:15:59 -0600

    setup test

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const app = require("express")();
+const http = require('http').Server(app);
+const express = require('express');
+const mysql = require('mysql');
+
+const PORT = process.env.PORT || 3456;
+
+
+const io = require("socket.io")(http, {
+  cors:{
+    origin: "http://localhost:3000",
+    methods: ["GET","POST"],
+  }
+});
+   
+
+io.on('connection', function(socket){
+    console.log("A client conencted "+ socket.id);
+   
+    socket.emit("printSocket", {socketid: socket.id});
+
+    socket.on('disconnect', function(){
+        console.log(`A client with id ${socket.id} disconnected`);
+    });
+});
+
+http.listen(PORT, function(err){
+    if(err){console.log(err)};
+    console.log(`Server listening on ${PORT}`);
+});
